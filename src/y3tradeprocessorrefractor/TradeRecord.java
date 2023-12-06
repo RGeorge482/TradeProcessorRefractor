fd7a31e382a0a5c2499718fe3c6b8fccff371fd4
RGeorge482 <ratagheorghita@yahoo.com>
2023-12-06 23:14:00 +0000

    TradeRecord
    TradeRecord class added

---
+++ src/y3tradeprocessorrefractor/TradeRecord.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package y3tradeprocessorrefractor;
+
+/**
+ *
+ * @author 35386
+ */
+public class TradeRecord {
+    private String sourceCurrencyCode;
+    private String destinationCurrencyCode;
+    private int tradeAmount;
+    private double tradePrice;
+    
+    
+
+    public TradeRecord(String sourceCurrencyCode, String destinationCurrencyCode, int tradeAmount, double tradePrice) {
+        this.sourceCurrencyCode = sourceCurrencyCode;
+        this.destinationCurrencyCode = destinationCurrencyCode;
+        this.tradeAmount = tradeAmount;
+        this.tradePrice = tradePrice;
+    }
+
+    public String getSourceCurrencyCode() {
+        return sourceCurrencyCode;
+    }
+
+    public void setSourceCurrencyCode(String sourceCurrencyCode) {
+        this.sourceCurrencyCode = sourceCurrencyCode;
+    }
+
+    public String getDestinationCurrencyCode() {
+        return destinationCurrencyCode;
+    }
+
+    public void setDestinationCurrencyCode(String destinationCurrencyCode) {
+        this.destinationCurrencyCode = destinationCurrencyCode;
+    }
+
+    public int getTradeAmount() {
+        return tradeAmount;
+    }
+
+    public void setTradeAmount(int tradeAmount) {
+        this.tradeAmount = tradeAmount;
+    }
+
+    public double getTradePrice() {
+        return tradePrice;
+    }
+
+    public void setTradePrice(double tradePrice) {
+        this.tradePrice = tradePrice;
+    }
+}
