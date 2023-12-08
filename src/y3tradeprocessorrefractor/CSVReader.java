17726c829165af6e0dc37ab883bb2ff063afdcbb
RGeorge482 <ratagheorghita@yahoo.com>
2023-12-08 22:00:49 +0000

    CSVReader and DataInput classes update

--- src/y3tradeprocessorrefractor/CSVReader.java
+++ src/y3tradeprocessorrefractor/CSVReader.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package y3tradeprocessorrefractor;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author 35386
  */
-public class CSVReader {
-    
+public class CSVReader implements DataInput {
+    BufferedReader myReader;
+    List<String> lines = new ArrayList<>();
+    public List<String> getData() throws IOException {
+       myReader = new BufferedReader(new FileReader("trades.csv"));
+       String inputLine = myReader.readLine();
+       while ((inputLine = myReader.readLine()) != null) {
+            lines.add(inputLine);
+       } 
+       return lines;
+    }
 }
--- src/y3tradeprocessorrefractor/DataInput.java
+++ src/y3tradeprocessorrefractor/DataInput.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package y3tradeprocessorrefractor;
 
-/**
- *
- * @author 35386
- */
+import java.io.IOException;
+import java.util.List;
+
+
 public interface DataInput {
-    
+    public List<String> getData() throws IOException;   
 }
